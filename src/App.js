dfa52007267137a200c818b5a64aa1b21533f615
Ariful <ariful@linkstaff.co.jp>
2021-06-18 11:01:44 +0900

    Shop page design done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import "./App.css";
 import Homepage from "./Component/HomePage/Homepage";
+import Shop from "./Component/Pages/Shop/Shop";
+import TopicList from "./Component/Topiclist/TopicList";
+
+// const TopicList = () => {
+//   return (
+//     <div>
+//       <h1></h1>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
-    <div className="App">
-      <h1>Happy Shopping!!</h1>
-      <Homepage />
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topic page</Link>
+          </li>
+          <li>
+            <Link to="/Shop">Shop Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />{" "}
+          </Route>
+          <Route exact path="/topics">
+            <TopicList />{" "}
+          </Route>
+          <Route exact path="/Shop">
+            <Shop />{" "}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Component/Topiclist/TopicList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopicList = () => {
+  return (
+    <div>
+      <h1>Topic List componenet!!</h1>
+    </div>
+  );
+};
+
+export default TopicList;
